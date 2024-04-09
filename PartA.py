b00f31ee1badeaf1535427e3f9b8a2322026f0e2
Jonathan Vigil <jvigil1738@gmail.com>
2024-04-09 13:08:29 -0700

    Feat: Part A modularized into 3 functions and Part B finished

--- PartA.py
+++ PartA.py
@@ -1,38 +1,70 @@
 import sys
 import os
 
-#have a current token working on and keep adding to it until break.
-# This fixes readline breaking up that character by keeping it in memory after the next readline
 def tokenize(filePath):
-    tokens = {}
+    tokens = []
     current_word = ""
+
     with open(filePath, 'r') as file:
         line = file.readline(100)
         while line:
-            print(line)
+
+            for char in line:
+                ord_char = ord(char)
+                if (ord_char >= 97 and ord_char <= 122) or (ord_char >= 48 and ord_char <= 57):
+                    current_word += char
+                elif ord_char >= 65 and ord_char <= 90:
+                    current_word += char.lower()
+                else:
+                    if current_word != "":
+                        tokens.append(current_word)
+                    current_word = ""
+
             line = file.readline(100)
 
+        if current_word:
+            tokens.append(current_word)
+
+    return tokens
+
 def computeWordFrequency(tokenList):
-    pass
+    token_map = {}
+    for token in tokenList:
+        if token in token_map:
+            token_map[token] += 1
+        else:
+            token_map[token] = 1
+    return token_map
 
 def printFrequencies(wordCount):
-    pass
+    sorted_list = [(key, count) for key, count in wordCount.items()]
+    sorted_list.sort(key=lambda x: x[0])
+    sorted_list.sort(key=lambda x: x[1], reverse=True)
+    
+    for token in sorted_list:
+        print(f'{token[0]} = {token[1]}')
 
-def checkArgs():
-    filepath = sys.argv[1]
+
+def checkArgs(filepath):
     if not os.path.isfile(filepath):
-        print("File path filePath does not exist. Exiting...".format(filepath))
-        sys.exit()
+        print(f"File path {filepath} does not exist. Exiting...")
+        return False
+    return True
 
 
 if __name__ == "__main__":
-    checkArgs()
-    tokenize(sys.argv[1])
     # perform argument check
-    # read in file
-    # tokenize in file
-    # computer word frequencies
-    # print frequencies
-    # handle additional errors
+    if checkArgs(sys.argv[1]):
+
+        # read in file and tokenize in file
+        tokens = tokenize(sys.argv[1])
+
+        # compute word frequencies
+        token_count = computeWordFrequency(tokens)
+
+        # print frequencies
+        printFrequencies(token_count)
+
+        # handle additional errors
 
     # to run: python3 partA.py test.txt
--- PartB.py
+++ PartB.py
@@ -1,10 +1,30 @@
 import PartA as A
+import sys
+import os
 
 def countCommonTokens(file1_tokens, file2_tokens):
+    list1 = set(file1_tokens)
+    list2 = set(file2_tokens)
+
+    if len(list1) > len(list2):
+        list1, list2 = list2, list1
+
+    common_token_count = 0
+    for token in list1:
+        if token in list2:
+            common_token_count += 1
+            print(token)
+    print(common_token_count)
+    return common_token_count
 
 if __name__ == "__main__":
     # perform argument checks
-    # read in files 
-    # tokenize files
-    # count common tokens and print them
-    # handle any additional errors
+    if A.checkArgs(sys.argv[1]) and A.checkArgs(sys.argv[2]):
+
+        # read in files and tokenize files
+        tokens1, tokens2 = A.tokenize(sys.argv[1]), A.tokenize(sys.argv[2])
+
+        # count common tokens and print them
+        countCommonTokens(tokens1, tokens2)
+
+        # handle any additional errors
